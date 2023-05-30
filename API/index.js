e5c7d5004fca860aaadb8d08958bf9883ea17e60
Thang Phan <thang.phan@internetbrands.com>
2023-05-30 04:56:06 -0700

    Fixed

--- API/index.js
+++ API/index.js
@@ -10,7 +10,6 @@ app.use(cors());
 
 app.post("/message", (req, res) => {
   const message = req.body.prompt.toLowerCase();
-  console.log(message);
   if (message == "how are you") {
     res.send({ message: "I am doing well. How about you?" });
   } else if (message == "how can i assign an article") {
@@ -19,13 +18,22 @@ app.post("/message", (req, res) => {
         "As a content manager >>> Go to Publication Tab >>> Open a book contains the article >>> Expand the chapter and click select the article >>> Click Statuses and choose Assign from the dropdown >>> Choose a writer and click Assign",
     });
   } else if (message == "how can i reset the changes i just made") {
-    res.send({ message: "I don't have an asnwer to your question. I will create a ticket to the Sequoia team and have them look at the issue. Can you please confirm by entering your email?" });
+    res.send({
+      message:
+        "I don't have an answer to your question. I will create a ticket to the Sequoia team and have them look at the issue. Can you please confirm by entering your email?",
+    });
   } else if (message == "thang.phan@internetbrands.com") {
-    res.send({ message: "Thank you, Thang! When the ticket is created, you will receive an email from Sequoia team." });
+    res.send({
+      message:
+        "Thank you, Thang! When the ticket is created, you will receive an email from Sequoia team.",
+    });
   } else if (message == "thank you") {
     res.send({ message: "My pleasure!" });
+  } else if (message == "how can i submit a partial chapter") {
+    res.send({
+      message: "You have to submit all of the assignments, not partially.!",
+    });
   } else {
-    console.log("here");
     res.send({ message: "Sorry, I dont understand." });
   }
 });
